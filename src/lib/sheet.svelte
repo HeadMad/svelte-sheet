9237bba71c0349320cd586f7eaa5bddc23450c02
HeadMad <kov.ps@ya.ru>
2024-09-24 16:51:26 +0800

    add selection by mousover

--- src/lib/sheet.svelte
+++ src/lib/sheet.svelte
@@ -9,8 +9,10 @@
   $: selection = fillAll(false);
 
   var active = null;
+  let isMouseDown = false;
 
-  function tableClick(event) {
+  function mouseDown(event) {
+    isMouseDown = true;
     const target = event.target;
     if (target.tagName !== 'TD') return;
 
@@ -70,6 +72,33 @@
     }
   }
 
+  function mouseUp() {
+    isMouseDown = false;
+  }
+
+  function mouseOver(event) {
+    if (!isMouseDown) return;
+    console.log('OVER')
+    const target = event.target;
+    if (target.tagName !== 'TD') return;
+    const rowI = target.parentNode.rowIndex - 1;
+    const colI = target.cellIndex - 1;
+
+    if (rowI === -1) {
+      if (colI === -1)
+        selection = fillAll(true);
+      else
+        selection = fillArea(fillAll(false), [0, colI], [rLen - 1, active ? active[1] : colI], true);
+      
+    } else if (colI === -1) {
+      selection = fillArea(fillAll(false), [active[0], 0], [rowI, cLen - 1], true);
+
+      
+    } else {
+      selection = fillArea(fillAll(false), [rowI, colI], active, true);
+    }
+  }
+
 </script>
 
 <!-- {JSON.stringify(data)} -->
@@ -79,7 +108,10 @@ use:outsideClick={() => {
   selection = fillAll(false);
   active = null;
 }}
-on:click={tableClick}
+
+on:mousedown={mouseDown}
+on:mouseup={mouseUp}
+on:mouseover={mouseOver}
 >
   <tr>
     <td></td>
@@ -117,7 +149,7 @@ on:click={tableClick}
   }
 
   td.selected {
-    background-color: #e6eef8;
+    background-color: #c2dbfa;
   }
 
   td.active {
